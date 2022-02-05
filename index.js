9f01b858b4808624a92530b58f5875f6ecc60832
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2022-02-05 11:18:58 -0300

    Modified query to filter if has phone

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ const persons = [
 
 const typeDefs = gql`
 
+    enum YesNo {
+        YES
+        NO
+    }
+
     type Address {
         street: String!
         city: String!
@@ -51,7 +56,7 @@ const typeDefs = gql`
 
     type Query {
         personCount: Int!
-        allPersons: [Person]!
+        allPersons(phone: YesNo): [Person]!
         findPerson(name: String!): Person
     }
 
@@ -71,7 +76,14 @@ la cantidad de personas, por eso la exclamación
 const resolvers = {
     Query: {
         personCount: () => persons.length,
-        allPersons: () => persons,
+        allPersons: (root, args) => {
+            if (!args.phone) return persons
+
+            const byPhone = person => 
+                args.phone === 'YES' ? person.phone : !person.phone
+            
+            return persons.filter(byPhone)
+        },
         findPerson: (root, args) => {
             const {name} = args
             return persons.find(person => person.name === name)
